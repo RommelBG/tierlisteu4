4ae4fc5bf4c1bc385d6f501df2cc26a3bebc377a
RommelBG <105991164+RommelBG@users.noreply.github.com>
2025-01-25 15:35:14 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -17,7 +17,8 @@ COULEURS_TIERS = {
 CLE_API = os.getenv('CLE_API', '') 
 
 # Chemin par défaut vers le dossier des drapeaux (modifiable via l'interface)
-CHEMIN_DRAPEAUX = "/var/www/tierlisteu4/flags"
+BASE_DIR = Path(__file__).resolve().parent
+CHEMIN_DRAPEAUX = BASE_DIR / "flags"
 
 # Mode local (True) ou production (False)
 # En local (http://localhost:8501), mettez True.
